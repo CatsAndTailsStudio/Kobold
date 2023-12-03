4182461cbdf328e6399ce50e28932972c98b641e
realtradam <github@tradam.dev>
2023-12-03 00:55:26 -0500

    added better template when "kobold init"ing

--- lib/Kobold/init.rb
+++ lib/Kobold/init.rb
@@ -9,12 +9,28 @@ module Kobold
     # create empty file with current
     # file version in current dir
     def init
-      kobold_config = TTY::Config.new
-      kobold_config.filename = ".kobold"
-      kobold_config.extname = ".ini"
-      kobold_config.set "_kobold_config", "format_version", value: Kobold::FORMAT_VERSION
-      kobold_config.write
-      File.rename ".kobold.ini", ".kobold"
+      kobold_default = <<-EOS
+[_kobold_config]
+format_version = #{Kobold::FORMAT_VERSION}
+
+; must be unique, can be anything that doesnt start with underscore
+;[raylib-linux]
+;
+; required
+;repo = raysan5/raylib
+;source = https://github.com
+;
+; optional, remove slash at the end to rename the dir the repo is in
+;dir = external/linux-x64/
+;
+; one of these 2 is required
+;branch = something
+;commit = 'b8cd102'
+;
+; optional, makes unique trunk
+;label = linux-x64
+      EOS
+      File.write('.kobold', kobold_default)
     end
   end
 end
--- lib/Kobold/version.rb
+++ lib/Kobold/version.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 module Kobold
-  VERSION = "0.3.3"
+  VERSION = "0.3.4"
   FORMAT_VERSION = "0.3.0"
+  class << self
+    def version
+      puts "Kobold:        #{Kobold::VERSION}"
+      puts "Kobold Format: #{Kobold::FORMAT_VERSION}"
+    end
+  end
 end
